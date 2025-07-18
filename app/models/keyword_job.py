29b5c148bed40987039d8e2a450340a0a8ead795
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-07-18 12:14:38 +0330

    create keyword_job association table for many-to-many relationship between Job and Keyword

---
+++ app/models/keyword_job.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, ForeignKey, Table, TIMESTAMP
+from sqlalchemy.sql import func
+from .base import Base
+
+keyword_job = Table(
+    "keyword_job",
+    Base.metadata,
+    Column("keyword_id", Integer, ForeignKey("keyword.id", ondelete="CASCADE"), primary_key=True),
+    Column("job_id", Integer, ForeignKey("job.id", ondelete="CASCADE"), primary_key=True),
+    Column("last_update", TIMESTAMP, nullable=False, server_default=func.now())
+)
