c7fbee440a3ba9284f4f5f7fb3c6ea0ef58f49fb
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-09-03 08:58:27 +0330

    add User model

---
+++ app/models/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from sqlalchemy.orm import relationship
+from database.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
