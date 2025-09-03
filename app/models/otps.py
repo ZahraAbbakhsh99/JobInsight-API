135f7f3b812fe289dbf71c6d8924fd69cbedba99
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-09-03 08:59:48 +0330

    add Otp model

---
+++ app/models/otps.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, func
+from sqlalchemy.orm import relationship
+from database.base import Base
+
+class Otp(Base):
+    __tablename__ = "otps"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    code = Column(String, nullable=False, index=True)
+    is_used = Column(Boolean, default=False)
+    expires_at = Column(DateTime(timezone=True), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="otps")
--- app/models/users.py
+++ app/models/users.py
@@ -10,3 +10,5 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    otps = relationship("Otp", back_populates="user")
