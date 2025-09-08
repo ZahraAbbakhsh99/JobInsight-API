83f8a2884290193fcb55a97fe5f6c6669e7ae38e
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-09-08 14:28:33 +0330

    implement download_jobs route with auth and CSV generation

--- app/api/routes/protected_routes.py
+++ app/api/routes/protected_routes.py
@@ -1,17 +1,57 @@
-from fastapi import APIRouter, Header, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, status
 from app.schemas.csv import *
-from app.utils.token_utils import *
+from app.schemas.job import *
+from sqlalchemy.orm import Session
+from app.services.auth import get_current_user
+from database.session import get_db
+from app.crud.keyword import get_keyword
+from app.crud.job import get_jobs_by_keyword
+from app.services.csv import *
+import uuid
 
 router = APIRouter(tags=["Jobs"])
 
 @router.post("/jobs/download", response_model=CSVResponse)
-async def download_jobs_csv(request: CSVRequest, 
-                            authorization: str = Header(..., description="Bearer token for authentication")):
+async def download_jobs_csv(request: CSVRequest,
+                            db: Session = Depends(get_db), 
+                            current_user = Depends(get_current_user)):
     """
     Generate a CSV file with job postings and 
     return a link to download the file.
     """
-    # the CSV should be created and saved on the server.
-    if not verify_token(authorization):
-        raise HTTPException(status_code=401, detail="Invalid token")
-    return {"download_link": "https://api.example.com/files/mock123.csv"}
+
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    keyword_result = get_keyword(db , request.keyword)
+    if keyword_result["status"]:
+        keyword_id = keyword_result["id"]
+    elif keyword_result["status"] == 0:
+       # It must be queued for processing...
+        if current_user:
+            return DetailResponse(
+                detail=f"Your request will be queued and you will be notified by email {current_user.email} once it has been processed."
+            )
+        else:
+            return DetailResponse(
+                detail="Your request cannot be processed at this time and has been placed in a queue."
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Keyword '{request.keyword}' not found"
+        )
+
+    if request.limit == 0:
+       return {"jobs": [], "keyword_id": request.keyword}
+    
+    jobs = get_jobs_by_keyword(db, keyword_id, request.limit)
+    if not jobs:
+        raise HTTPException(status_code=404, detail="No jobs found for this keyword")
+    
+    filename = f"jobs_{request.keyword}_{uuid.uuid4().hex}.csv"
+    filepath = save_jobs_to_csv(jobs, filename)
+
+    download_link = f"/files/{filename}"
+
+    return CSVResponse(download_link=download_link)
